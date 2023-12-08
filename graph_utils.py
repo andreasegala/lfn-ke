975832137f3d7000851291da70c19ab5058ecd5c
Mariafiore <mariafiore.tognon@gmail.com>
2023-12-08 17:19:02 +0100

    Added implementation of approximated PageRank, minor updates on stopwords list

--- graph_utils.py
+++ graph_utils.py
@@ -28,8 +28,8 @@ class GraphMaker:
         
         font_size=26
         words_in_text = []
-        is_noun = lambda word, pos: pos[:2] == 'NN' and (word not in self.stopwords)
-        is_adjective_or_verb = lambda word , pos: (pos[:2]=='JJ' or pos[:2]=='VB') and (word not in self.stopwords)
+        is_noun = lambda word, pos: pos[:2] == 'NN' and not (word in self.stopwords)
+        is_adjective_or_verb = lambda word , pos: (pos[:2]=='JJ' or pos[:2]=='VB') and not (word in self.stopwords)
 
         for sent in text.split('.')[:-1]:
             tokenized = nltk.word_tokenize(sent)
@@ -45,7 +45,7 @@ class GraphMaker:
                 if self.stemmer.stem(word) not in words_list:
                     words_list.append(self.stemmer.stem(word))
 
-        df = pd.DataFrame(np.zeros(shape=(len(words_list),2)), columns=['Stems', 'Neighbours'])
+        df = pd.DataFrame(np.zeros(shape=(len(words_list),2)), columns=['Stems', 'neighbors'])
         df['Stems'] = words_list
         
         '''for sent in text.split('.'):
@@ -63,9 +63,9 @@ class GraphMaker:
             for stem in words_list:
                 if stem in stemmed_sent:
                     ind = df[df['Stems']==stem].index[0]
-                    #temp = df.at[ind, 'Neighbours']
-                    #df.at[ind, 'Neighbours']=temp.append([ss for (ss)in stemmed_sent if not(ss==stem)])
-                    df['Neighbours'][ind]=[ss for ss in stemmed_sent if not(ss==stem)]
+                    #temp = df.at[ind, 'neighbors']
+                    #df.at[ind, 'neighbors']=temp.append([ss for (ss)in stemmed_sent if not(ss==stem)])
+                    df['neighbors'][ind]=[ss for ss in stemmed_sent if not(ss==stem)]
 
         fig = plt.figure(figsize=(30,20))
         G = nx.Graph()
@@ -73,7 +73,7 @@ class GraphMaker:
         for i in range(len(df)):
             G.add_node(df['Stems'][i])
             color_map.append('blue')
-            for word in df['Neighbours'][i]:
+            for word in df['neighbors'][i]:
                 G.add_edges_from([(df['Stems'][i], word)])
 
         pos = nx.spring_layout(G, k)
@@ -99,6 +99,60 @@ class GraphMaker:
         plt.show()
 
         return G   
+
+def localPageRankApprox(G) -> dict:
+    nodes_list = G.nodes
+    n = len(nodes_list)
+    alpha = 0.85
+    computed_centralities =  {} #dizionario per le centralità calcolate
+    r = 10
+    for u in nodes_list:
+        #initializing for t=0
+        PR_u = np.zeros(r+1)
+        PR_u[0] = 1
+        layers = np.empty(r+1, dtype =dict)
+        layers[0] = {}
+        layers[0][u] = 1
+        inf_dict = np.empty(r+1, dtype= dict) #at each lvl there will be inf_t(x,u) with key x and value u 
+        inf_dict[0] ={}
+        inf_dict[0][u] =1
+        for t in range(1,r+1):
+            layers[t]={}
+            inf_dict[t] ={}
+            #initializing layer t: we put in layer t only neighbors of nodes in layers[t-1] che non sono già stati visitati: elif caso 0
+            last_visited = layers[t-1]
+            for v in last_visited:
+                for w in G.neighbors(v):
+                    #se w è un vicino che non è ancora stato visitato
+                    if t>1:
+                        if w not in last_visited and not w in layers[t-2]:
+                            layers[t][w] = 1
+                    else:
+                        layers[t][w]=1
+            #ho inizializzato il layer t
+            for v in layers[t]:
+                s=0
+                for w in layers[t-1]:
+                    if w in G.neighbors(v):
+                        s += inf_dict[t-1][w]
+                inf_dict[t][v] = (1/G.degree[v]) * s
+            
+            totSum = sum(layers[t][key] for key in layers[t])
+            PR_u[t] = PR_u[t-1] + ((1-alpha)*(alpha**t)/n)* totSum
+        computed_centralities[u] = PR_u[r]
+    return computed_centralities
+
+                
+
+                    
+            
+
+    
+        
+
+
+        
+
 
         
     
