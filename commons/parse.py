0384a1f7077f35015d478880233cb708d20ceeea
andre <andrea.segala9@gmail.com>
2024-01-23 23:27:34 +0100

    Updated allowed files function, minor changes

--- commons/parse.py
+++ commons/parse.py
@@ -9,44 +9,45 @@ import commons.graph
 import nltk
 
 
-def update_allowed_forbidden_files():
+def update_allowed_forbidden_files(graphmaker, min_nodes):
     # get all the filenames
     filenames = Path("../data/elsevier_oaccby_corpus/json/").glob("*.json")
 
     # lists to store allowed and forbidden files
     allowed_files = []
-    forbidden_files = []
 
     # required keys for a document to be valid
     required_keys = ['docId', 'abstract']
     required_metadata_keys = ['keywords', 'subjareas']
 
-    # TODO se volete tqdm si può togliere, l'avevo usato per vedere l'avanzamento (fa la progress bar)
     # for each file, check if there are all the required keys and mark the file as allowed
-    # if not, mark the file as forbidden
+    # and check that the largest connected component has at least min_nodes nodes
     for filename in tqdm(filenames, total=40001):
         # load JSON file
         file = open(filename, 'r')
         json_file = json.load(file)
         file.close()
 
-        # check the keys and mark as allowed or forbidden
-        # it saves whole relative paths (see line 13)
+        # check the keys and the larget connected component of the graph
+        # and marks the file as allowed or forbidden
+        # it saves whole relative paths
+        is_allowed = True
         if all(key in json_file for key in required_keys) and all(key in json_file['metadata'] for key in required_metadata_keys):
-            allowed_files.append(str(filename))
+            article_graph = graphmaker.buildGraph(json_file['abstract'])
+            largest_connected_component = len(max(list(nx.connected_components(article_graph)), key=len))
+            if largest_connected_component < min_nodes:
+                is_allowed = False
         else:
-            forbidden_files.append(str(filename))
+            is_allowed = False
 
-    # write allowed file into a .txt file
+        # if the file is allowed, append it to the corresponding list
+        if is_allowed:
+            allowed_files.append(str(filename))
+
+    # write allowed files into a .txt file
     allowed_writer = open(Path('resources/allowed_files.txt'), 'w')
     allowed_writer.write('\n'.join(allowed_files))
     allowed_writer.close()
-
-    # write forbidden file into a .txt file
-    # TODO (vedi linea 75) ma questo serve se salviamo gli allowed?
-    forbidden_writer = open(Path('resources/forbidden_files.txt'), 'w')
-    forbidden_writer.write('\n'.join(forbidden_files))
-    forbidden_writer.close()
 
 
 class Article:
@@ -57,10 +58,12 @@ class Article:
         self.categories = categories
         self.filename = filename
 
+        # take only largest connected component
         article_graph = graphmaker.buildGraph(abstract)
         largest_connected_component = max(list(nx.connected_components(article_graph)), key=len)
         self.graph = article_graph.subgraph(largest_connected_component).copy()
 
+        # store keywords already stemmed
         kws = set()
         for keyword in keywords:
             tokens = nltk.word_tokenize(keyword)
@@ -68,21 +71,18 @@ class Article:
                 kws.add(graphmaker.stemmer.stem(token))
         self.kw = list(kws)
 
+    # TODO ma serve?
     def setGraph(self, graphmaker) -> None:
         self.graph = graphmaker.buildGraph(self.abstract)
 
     def printGraph(self):
         commons.graph.printGraph(self.graph)
 
-    def toString(self) -> str:
-        return ''
-
 
 def parse_and_sample(sample_size, graphmaker) -> list: # list of Article objs
     sampled_articles = []
     random.seed(3101960)
 
-    # TODO (vedi linea 45) serve avere forbidden list se abbiamo allowed list?
     # sample sample_size paths from the allowed ones
     with open(Path('resources/allowed_files.txt'), 'r') as handler:
         allowed_paths = [Path(line.rstrip()) for line in handler.readlines()]
@@ -102,12 +102,8 @@ def parse_and_sample(sample_size, graphmaker) -> list: # list of Article objs
             filename = path.name
 
             # create Article and append to sampled list
-            # TODO remove try catch
-            try:
-                new_article = Article(docID, abstract, categories, keywords, filename, graphmaker)
-                sampled_articles.append(new_article)
-            except:
-                print("file", str(path))
+            new_article = Article(docID, abstract, categories, keywords, filename, graphmaker)
+            sampled_articles.append(new_article)
 
     return sampled_articles
 
